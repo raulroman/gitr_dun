e9cac2ddeef2dd1ec606dd475b652ec9fb7a6d06
RaulRoman <raulromanjr@gmail.com>
2017-12-02 08:18:18 -0500

    add alt method style

--- select_movie.rb
+++ select_movie.rb
@@ -1,3 +1,4 @@
+#Use the `select` method with the following array of hashes
 movies = [
     {id: 1, title: "Die Hard", rating: 4.0},
     {id: 2, title: "Bad Boys", rating: 5.0},
@@ -5,10 +6,14 @@ movies = [
     {id: 4, title: "Fracture", rating: 2.0}
 ]
 
-# Use the `select` method with the following array of hashes
+# to create a new array of hashes that only contain movies with a rating less than 4.0.
+result = movies.select { |movie| movie[:rating]< 4.0 }
+p result
+
+#It can also be done this way as a do and end
 
 result = movies.select do |movie| 
   movie[:rating] < 4.0
 end
-#to create a new array of hashes that only contain movies with a rating less than 4.0.
 p result
+
